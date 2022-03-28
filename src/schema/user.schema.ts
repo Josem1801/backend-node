ca299aa95192e4b6799851aef010751edb753fe7
Jose Manuel Rosales <josemanuelrosales2@gmail.com>
2022-03-28 12:26:37 -0600

    ✅ user schema validation

---
+++ src/schema/user.schema.ts
@@ -0,0 +1,20 @@
+import Joi from "joi";
+
+const id = Joi.number().integer();
+const email = Joi.string().email();
+const password = Joi.string().min(8);
+
+
+
+export const createUserSchema = Joi.object({
+	email: email.required(),
+	password: password.required(),
+});
+
+export const updateUserSchema = Joi.object({
+	email: email.required(),
+});
+
+export const getUserSchema = Joi.object({
+	id: id.required()
+});
